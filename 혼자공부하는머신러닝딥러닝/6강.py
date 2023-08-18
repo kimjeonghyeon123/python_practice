c1efb62ccd90927d087d304555f3b7be18616ec9
kimjeonghyeon <gojhkim123456@gmail.com>
2023-08-18 22:27:20 +0900

    추가

--- 혼자공부하는머신러닝딥러닝/6강.py
+++ 혼자공부하는머신러닝딥러닝/6강.py
@@ -10,25 +10,29 @@ K-최근접 이웃
     - K-최근접 이웃 회귀
         - 근처 타깃들의 평균
 '''
+import numpy as np
 
-bream_length = [25.4, 26.3, 26.5, 29.0, 29.0,
-                29.7, 29.7, 30.0, 30.0, 30.7,
-                31.0, 31.0, 31.5, 32.0, 32.0,
-                32.0, 33.0, 33.0, 33.5, 33.5,
-                34.0, 34.0, 34.5, 35.0, 35.0,
-                35.0, 35.0, 36.0, 36.0, 37.0,
-                38.5, 38.5, 39.5, 41.0, 41.0]
-bream_weight = [242.0, 290.0, 340.0, 363.0, 430.0,
-                450.0, 500.0, 390.0, 450.0, 500.0,
-                475.0, 500.0, 500.0, 340.0, 600.0,
-                600.0, 700.0, 700.0, 610.0, 650.0,
-                575.0, 685.0, 620.0, 680.0, 700.0,
-                725.0, 720.0, 714.0, 850.0, 1000.0,
-                920.0, 955.0, 925.0, 975.0, 950.0]
+perch_length = np.array(
+    [8.4, 13.7, 15.0, 16.2, 17.4, 18.0, 18.7, 19.0, 19.6, 20.0,
+     21.0, 21.0, 21.0, 21.3, 22.0, 22.0, 22.0, 22.0, 22.0, 22.5,
+     22.5, 22.7, 23.0, 23.5, 24.0, 24.0, 24.6, 25.0, 25.6, 26.5,
+     27.3, 27.5, 27.5, 27.5, 28.0, 28.7, 30.0, 32.8, 34.5, 35.0,
+     36.5, 36.0, 37.0, 37.0, 39.0, 39.0, 39.0, 40.0, 40.0, 40.0,
+     40.0, 42.0, 43.0, 43.0, 43.5, 44.0]
+     )
+perch_weight = np.array(
+    [5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0,
+     110.0, 115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0,
+     130.0, 150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0,
+     197.0, 218.0, 300.0, 260.0, 265.0, 250.0, 250.0, 300.0, 320.0,
+     514.0, 556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0,
+     820.0, 850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0,
+     1000.0, 1000.0]
+     )
 
 import matplotlib.pyplot as plt
 
-plt.scatter(bream_length, bream_weight)
+plt.scatter(perch_length, perch_weight)
 plt.xlabel('length')
 plt.ylabel('weight')
 plt.show()
@@ -36,15 +40,14 @@ plt.show()
 # 훈련 세트 준비
 # 전부 1차원 배열로 나옴
 # 훈련 데이터는 2차원, 타겟 데이터는 1차원
-import numpy as np
-length = np.array(bream_length).reshape(-1, 1)
-weight = np.array(bream_weight)
 
 from sklearn.model_selection import train_test_split
 train_input, test_input, train_target, test_target = train_test_split(
-    length, weight, random_state=42
+    perch_length, perch_weight, random_state=42
 )
 
+train_input = train_input.reshape(-1, 1)
+test_input = test_input.reshape(-1, 1)
 # 회귀 모델 훈련
 from sklearn.neighbors import KNeighborsRegressor
 
@@ -76,12 +79,6 @@ print(knr.score(test_input, test_target))
 # 기본값은 5임
 knr.n_neighbors = 3
 print('3일 때')
-print(knr.score(train_input, train_target))
-print(knr.score(test_input, test_target))
-
-# 과대적합이므로 이웃의 개수를 높여야 됨
-knr.n_neighbors = 6
-print('6일 때')
 print(knr.score(train_input, train_target))
 print(knr.score(test_input, test_target))
 
