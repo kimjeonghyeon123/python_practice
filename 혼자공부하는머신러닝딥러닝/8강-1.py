d493cdfd18a44f56cd2280e1dcafd554e73744b9
kimjeonghyeon <gojhkim123456@gmail.com>
2023-08-20 19:48:01 +0900

    추가

---
+++ 혼자공부하는머신러닝딥러닝/8강-1.py
@@ -0,0 +1,85 @@
+# pandas로 csv 파일 가져오고 numpy 배열로 변환
+import pandas as pd
+
+df = pd.read_csv('https://bit.ly/perch_csv')
+perch_full = df.to_numpy()
+
+import numpy as np
+
+perch_weight = np.array(
+    [5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0,
+     110.0, 115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0,
+     130.0, 150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0,
+     197.0, 218.0, 300.0, 260.0, 265.0, 250.0, 250.0, 300.0, 320.0,
+     514.0, 556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0,
+     820.0, 850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0,
+     1000.0, 1000.0]
+     )
+
+# 나누기
+from sklearn.model_selection import train_test_split
+
+train_input, test_input, train_target, test_target = train_test_split(
+    perch_full, perch_weight, random_state=42
+)
+
+# 변환기로 특성 개수 늘리기
+from sklearn.preprocessing import PolynomialFeatures
+
+poly = PolynomialFeatures(include_bias=False)
+poly.fit(train_input)
+train_poly = poly.transform(train_input)
+test_poly = poly.transform(test_input)
+
+print(train_poly.shape)
+print(poly.get_feature_names_out())
+
+# 회귀시키기
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+lr.fit(train_poly, train_target)
+print(lr.score(train_poly, train_target))
+print(lr.score(test_poly, test_target))
+
+# 더 많은 특성 만들기
+
+poly = PolynomialFeatures(degree=5, include_bias=False)
+poly.fit(train_input)
+train_poly = poly.transform(train_input)
+test_poly = poly.transform(test_input)
+
+lr.fit(train_poly, train_target)
+print(lr.score(train_poly, train_target))
+print(lr.score(test_poly, test_target))
+
+# 데이터 스케일링하기
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+ss.fit(train_poly)
+train_scaled = ss.transform(train_poly)
+test_scaled = ss.transform(test_poly)
+
+# 릿지 회귀
+from sklearn.linear_model import Ridge
+ridge = Ridge()
+ridge.fit(train_scaled, train_target)
+print(ridge.score(train_scaled, train_target))
+print(ridge.score(test_scaled, test_target))
+
+alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
+train_score = []
+test_score = []
+for alpha in alpha_list:
+    ridge = Ridge(alpha=alpha)
+    ridge.fit(train_scaled, train_target)
+    train_score.append(ridge.score(train_scaled, train_target))
+    test_score.append(ridge.score(test_scaled, test_target))
+
+import matplotlib.pyplot as plt
+
+plt.plot(np.log10(alpha_list), train_score)
+plt.plot(np.log10(alpha_list), test_score)
+plt.xlabel('alpha')
+plt.ylabel('R^2')
+plt.show()
